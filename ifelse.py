e32a36c180616d84a7a3b3bc1e7a3a78186a677d
Liberbell <liberbell@gmail.com>
2020-06-04 18:08:12 +0900

    ifelse4

--- ifelse.py
+++ ifelse.py
@@ -4,3 +4,14 @@ if 10 > 20:
 else:
     print("10 is less than 20")
     print("else block activated.")
+
+bike_price = 7160
+print(bike_price)
+
+if bike_price <= 8000:
+    print("It`s a cheap bike.")
+    print("If block activated.")
+
+else:
+    print("It`s a expensive bike.")
+    print("Else block activated.")
