a61ca6a433f85aae91fffb81196f8298508e23c7
Liberbell <liberbell@gmail.com>
2020-06-07 14:04:00 +0900

    ifdates2

--- ifdates.py
+++ ifdates.py
@@ -1,3 +1,6 @@
 fruit_list = ["apple", "orange", "grape", "banana", "avocado"]
 if "grape" in fruit_list:
     print("Yes, grape is in the fruit list.")
+
+else:
+    print("No, grape is not in the fruit list.")
