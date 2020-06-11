8e425e137875b5d2b2c827a3d49f0ae3c0d72902
Liberbell <liberbell@gmail.com>
2020-06-12 08:47:11 +0900

    typeconv2-6

--- typeconv2.py
+++ typeconv2.py
@@ -23,3 +23,6 @@ print("Type of value_str is: ", type(value_str))
 print("Type of valeu_list is: ", type(value_list))
 
 list((1, 2, 3, 4, 5))
+
+my_tuple = ["Apple", "Orage", "Mango", "Banana"]
+print(my_tuple)
