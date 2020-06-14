0a8aa42731ce190616a050fe3d91fa7ba032e722
Liberbell <liberbell@gmail.com>
2020-06-14 10:39:20 +0900

    basicpro4

--- basicpro.py
+++ basicpro.py
@@ -6,3 +6,9 @@ print("My total savings in a month is: ", saving)
 
 first_num = int(input("Enter first variable: "))
 second_num = int(input("Enter second variable: "))
+
+first_num = first_num + second_num
+second_num = first_num - second_num
+first_num = first_num - second_num
+
+print("\nFirst num is: ", first_num, "\nSecond num is: ", second_num)
