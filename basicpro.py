f190f0e22d74fa38034b665772133ce2a79ab525
Liberbell <liberbell@gmail.com>
2020-06-14 10:35:54 +0900

    basicpro3

--- basicpro.py
+++ basicpro.py
@@ -3,3 +3,6 @@ expenses = int(input("Enter your expenses: "))
 
 saving = salary - expenses
 print("My total savings in a month is: ", saving)
+
+first_num = int(input("Enter first variable: "))
+second_num = int(input("Enter second variable: "))
