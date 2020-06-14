c6206d58cc8d065e774506e1cd6571dd8cf6def0
Liberbell <liberbell@gmail.com>
2020-06-15 08:49:29 +0900

    basicpro3

--- basicpro.py
+++ basicpro.py
@@ -13,11 +13,15 @@
 
 # print("\nFirst num is: ", first_num, "\nSecond num is: ", second_num)
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-tmp1 = str(num)
-tmp2 = tmp1 + tmp1
-tmp3 = tmp1 + tmp1 + tmp1
+# tmp1 = str(num)
+# tmp2 = tmp1 + tmp1
+# tmp3 = tmp1 + tmp1 + tmp1
 
-total = num + (int(tmp2)) + (int(tmp3))
-print(tmp1, "+", tmp2, "+", tmp3, "=", total)
+# total = num + (int(tmp2)) + (int(tmp3))
+# print(tmp1, "+", tmp2, "+", tmp3, "=", total)
+
+num_1 = int(input("Enter first num: "))
+num_2 = int(input("Enter second num: "))
+Quotient = int(num_1 / num_2)
