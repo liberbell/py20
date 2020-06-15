f8e212e72a6a246b74324c3fc3c97fd69d3407a9
Liberbell <liberbell@gmail.com>
2020-06-16 08:57:05 +0900

    basicpro7

--- basicpro.py
+++ basicpro.py
@@ -69,3 +69,5 @@ print("Number of student: ", len(list_students))
 student_set = set(list_students)
 print("\nNew student list: ", student_set)
 print("\nNew number of student: ", len(student_set))
+
+print("There are %s duplicate elements.", len(list_students) - len(student_set))
