1fdb3a23d80601162553c992132a71614c3e462c
Liberbell <liberbell@gmail.com>
2020-06-19 08:52:52 +0900

    basicpro2-2

---
+++ basicpro2.py
@@ -0,0 +1,10 @@
+human_age = int(input("Enter dog's age in human years: "))
+if human_age < 0:
+    print("Age must be a positive number.")
+    exit()
+if human_age <= 2:
+    dog_age = human_age * 10.5
+
+else:
+    dog_age = 21 + (human_age - 2) * 4
+print("The dog's age in numan years is ", dog_age)
