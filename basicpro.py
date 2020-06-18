1ea71a4300b75e09583a3169019e3a359cb676d7
Liberbell <liberbell@gmail.com>
2020-06-18 09:34:38 +0900

    basicpro16

--- basicpro.py
+++ basicpro.py
@@ -183,3 +183,5 @@ if num_1 % 5 == 0 and num_1 % 7 == 0:
     print("This number is divisible by both 5 and 7")
 elif num_1 % 5 == 0:
     print("This number is divisible by 5")
+elif num_1 % 7 == 0:
+    print("This number is divisible by 7")
