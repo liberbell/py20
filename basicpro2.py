631f5c44ee9e82b83be60319fac5797d0d948273
Liberbell <liberbell@gmail.com>
2020-06-20 22:00:13 +0900

    basicpro2-4

--- basicpro2.py
+++ basicpro2.py
@@ -33,3 +33,8 @@
 a = int(input("a: "))
 b = int(input("b: "))
 c = int(input("c: "))
+
+if a == b == c:
+    print("Equilateral triangle")
+elif a == b or b == c or c == a:
+    print("Isosceles triangle")
